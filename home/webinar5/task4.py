c39e8e9b7bcf973b742d36f2682129862dcaee41
dzhumayev <dzhumayev@gmail.com>
2022-10-28 15:32:11 +0600

    v2

--- home/webinar5/task4.py
+++ home/webinar5/task4.py
@@ -3,18 +3,30 @@
 import re
 
 def rle_compress(text: str):
-    text = re.sub(r"(\S)(\d+)", r"\g<1>!\g<2>", text)
-    matches_nospace_series = re.findall(r"(([\S ])\2{2,})", text)
+    text = re.sub(r"(#)(\d+)", r"\g<1>_\g<2>", text)
+    matches_series = re.findall(r"(([\S ])\2{3,})", text)
 
-    for detected in matches_nospace_series:
-        series = detected[0]
-        symbol = detected[1]
-        text = re.sub(series, f"{symbol}{len(series)}", text)
+    for match in matches_series:
+        series = match[0]
+        symbol = match[1]
+        repl = f"#{len(series)}{symbol}"
+        text = re.sub(series, repl, text)
 
     return text
 
 def rle_decompress(text:str):
-    pass
+    patterns = [r"(([^_])(\d+))", r"(?<!_)(([_])(\d+))"]
+
+    for pattern in patterns:
+        matches = re.findall(pattern, text)
+        for match in matches:
+            code = match[0]
+            symbol = match[1]
+            length = int(match[2])
+            repl = symbol * length
+            text = re.sub(code, repl, text)
+
+    return re.sub(r"(?<=[\S ])(_)(?=\d+)", "", text)
 
 #with open("task4.input", encoding="utf-8") as f:
 #    text = f.readline()
--- home/webinar5/task4_test.py
+++ home/webinar5/task4_test.py
@@ -4,13 +4,13 @@ import task4
 
 class Test(TestCase):
     def test_rele_compress(self):
-        input = "Съешьььь   ееееещё этииииииих мяягких фффранцузских булок, да выпей же чаю а4"
+        input = "Съешьььь ееееещё этииииииих мяягких фффранцузских булок, да выпей же чаю #1а"
         actual = task4.rle_compress(input)
-        expected = "Съешь4 3е5щё эти7х мяягких ф3ранцузских булок, да выпей же чаю а!4"
-        self.assertEqual(actual, expected)
+        expected = "Съеш#4ь #5ещё эт#7их мяягких фффранцузских булок, да выпей же чаю #_1а"
+        self.assertEqual(expected, actual)
 
-    #def test_rle_decompress(self):
-    #    input = "Съешь ещё этих мягких французских булок, дабва абввыпейабв жеабв абвчаю"
-    #    actual = task1.remove_abv_words(input)
-    #    expected = "Съешь ещё этих мягких французских булок,"
-    #    self.fail()
+    def test_rle_decompress(self):
+        input = "Съеш#4ь #5ещё эт#7их мяягких фффранцузских булок, да выпей же чаю #_1а"
+        actual = task4.rle_decompress(input)
+        expected = "Съешьььь ееееещё этииииииих мяягких фффранцузских булок, да выпей же чаю #1а"
+        self.assertEqual(expected, actual)
