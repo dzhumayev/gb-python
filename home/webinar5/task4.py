df2e4037b35a494abef88cd866bf3acc2a7b113c
dzhumayev <dzhumayev@gmail.com>
2022-10-28 20:15:32 +0600

    webinar5 part1

--- home/webinar5/task4.py
+++ home/webinar5/task4.py
@@ -4,8 +4,10 @@ import re
 
 
 def rle_compress(text: str):
-    escape = '#'
-    text = re.sub(fr"({escape})(\d+)", r"\g<1>_\g<2>", text)
+    anchor = '#'
+    escape = '_'
+
+    text = re.sub(fr"({anchor})({escape})*(\d+)", fr"\g<1>\g<2>{escape}\g<3>", text)
 
     pattern = r"([\S ])\1{3,}"
     matches = [e for e in re.finditer(pattern, text)]
@@ -18,7 +20,7 @@ def rle_compress(text: str):
         result.append(slice)
         symbol = match.group(1)
         length = match.end(0) - match.start(0)
-        result.append(f"{escape}{length}{symbol}")
+        result.append(f"{anchor}{length}{symbol}")
         slice_since = position + length
         pass
 
@@ -26,18 +28,27 @@ def rle_compress(text: str):
 
 
 def rle_decompress(text: str):
-    patterns = [r"(([^_])(\d+))", r"(?<!_)(([_])(\d+))"]
+    anchor = '#'
+    escape = '_'
 
-    for pattern in patterns:
-        matches = re.findall(pattern, text)
-        for match in matches:
-            code = match[0]
-            symbol = match[1]
-            length = int(match[2])
-            repl = symbol * length
-            text = re.sub(code, repl, text)
+    pattern = fr"({anchor})(\d+)([\S ])"
+    matches = [e for e in re.finditer(pattern, text)]
 
-    return re.sub(r"(?<=[\S ])(_)(?=\d+)", "", text)
+    result = []
+    slice_since = 0
+    for match in matches:
+        position = match.start(1)
+        slice = text[slice_since:position]
+        result.append(slice)
+        symbol = match[3]
+        length = int(match[2])
+        repl = symbol * length
+        result.append(repl)
+        slice_since = match.end(3)
+        pass
+
+    result = "".join(result) + text[slice_since:]
+    return re.sub(fr"({anchor})({escape})", anchor, result)
 
 # with open("task4.input", encoding="utf-8") as f:
 #    text = f.readline()
--- home/webinar5/task4_test.py
+++ home/webinar5/task4_test.py
@@ -3,14 +3,14 @@ from unittest import TestCase
 import task4
 
 class Test(TestCase):
-    def test_rele_compress(self):
-        input = "ССССъешьььь ееееещё этииииииих мяягких фффранцузских булок, да выпей же чаю #1а"
+    def test_rle_compress(self):
+        input = "ССССъешьььь ееееещё этииииииих #_1а мяягких фффранцузских булок, да выпей же чаю #1а аааа"
         actual = task4.rle_compress(input)
-        expected = "#4Съеш#4ь #5ещё эт#7их мяягких фффранцузских булок, да выпей же чаю #_1а"
+        expected = "#4Съеш#4ь #5ещё эт#7их #__1а мяягких фффранцузских булок, да выпей же чаю #_1а #4а"
         self.assertEqual(expected, actual)
 
     def test_rle_decompress(self):
-        input = "Съеш#4ь #5ещё эт#7их мяягких фффранцузских булок, да выпей же чаю #_1а"
+        input = "#4Съеш#4ь #5ещё эт#7их #__1а мяягких фффранцузских булок, да выпей же чаю #_1а #4а"
         actual = task4.rle_decompress(input)
-        expected = "Съешьььь ееееещё этииииииих мяягких фффранцузских булок, да выпей же чаю #1а"
+        expected = "ССССъешьььь ееееещё этииииииих #_1а мяягких фффранцузских булок, да выпей же чаю #1а аааа"
         self.assertEqual(expected, actual)
