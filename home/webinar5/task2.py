5fec1b13eb54c094095b1563f9d08b7392f461c2
dzhumayev <dzhumayev@gmail.com>
2022-11-11 04:04:37 +0600

    webinar5 complete v3

--- home/webinar5/task2.py
+++ home/webinar5/task2.py
@@ -13,7 +13,7 @@ move_transferred_to_player = randint(1, 2)
 last_move_made_player = move_transferred_to_player
 
 
-def ask_about_of_candies_count():
+def ask_human_about_of_candies_count():
     while True:
         take_amount = int(input("Выберете количество конфет:"))
         if take_amount < 1 or take_amount > take_limit:
@@ -24,19 +24,19 @@ def ask_about_of_candies_count():
             continue
         return take_amount
 
+def ask_bot_about_of_candies_count():
+    if candies_amount[0] <= take_limit:
+        return candies_amount[0]
+    take_amount = candies_amount[0] % (take_limit + 1)
+    if take_amount == 0:
+        take_amount = randint(1, take_limit)
+    return take_amount
+
 
 def take_candies(mode):
-    if mode == "human":
-        take_amount = ask_about_of_candies_count()
-        candies_amount[0] = candies_amount[0] - take_amount
-        return take_amount
-
-    if mode == "bot":
-        take_amount = candies_amount[0] % (take_limit + 1)
-        if take_amount == 0:
-            take_amount = randint(1, take_limit) if candies_amount[0] >= take_limit else take_limit
-        candies_amount[0] = candies_amount[0] - take_amount
-        return take_amount
+    take_amount = ask_human_about_of_candies_count() if mode == "human" else ask_bot_about_of_candies_count()
+    candies_amount[0] = candies_amount[0] - take_amount
+    return take_amount
 
 
 while True:
@@ -46,17 +46,9 @@ while True:
         print(f"Победу одержал игрок {last_move_made_player}")
         break
 
-    if move_transferred_to_player == 1:
-        print("Ход делает игрок 1")
-        print(f"Игроком взято конфет в количестве: {take_candies('human')}")
-        print(f"Осталось конфет: {candies_amount[0]}\n")
-        last_move_made_player = 1
-        move_transferred_to_player = 2
-
-    if move_transferred_to_player == 2:
-        print("Ход делает игрок 2")
-        mode = "bot" if player2_is_bot else "human"
-        print(f"Игроком взято конфет в количестве: {take_candies(mode)}")
-        print(f"Осталось конфет: {candies_amount[0]}\n")
-        last_move_made_player = 2
-        move_transferred_to_player = 1
+    print(f"Ход делает игрок {'1' if move_transferred_to_player == 1 else '2'}")
+    mode = "bot" if move_transferred_to_player == 2 and player2_is_bot else "human"
+    print(f"Игроком взято конфет в количестве: {take_candies(mode)}")
+    print(f"Осталось конфет: {candies_amount[0]}\n")
+    last_move_made_player = 1 if move_transferred_to_player == 1 else 2
+    move_transferred_to_player = 1 if move_transferred_to_player == 2 else 2
